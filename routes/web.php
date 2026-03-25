3ad055a3c17b5183493bda84cbb10bc9f15043ab
nyapakid <putrayudha1412@unsri.ac.id>
2026-03-25 09:35:08 +0700

    1st build /blakang index

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+//depan
 Route::get('/', function () {
     return view('welcome');
+});
+
+//belakang
+Route::get('/belakang', function () {
+    return view('belakang.index');
 });
