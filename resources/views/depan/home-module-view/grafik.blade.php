7d56ba16cb716a540dc7d0cdf0a20a8fb8bdf03a
nyapakid <putrayudha1412@unsri.ac.id>
2026-04-23 12:47:13 +0700

    infografis naik sebagian

--- resources/views/depan/home-module-view/grafik.blade.php
+++ resources/views/depan/home-module-view/grafik.blade.php
@@ -4,13 +4,13 @@
         <h3>Infografis</h3>
           
         <!-- Owl Carousel-->
-        <div><img src="assets/depan/images/gallery-image-1-270x195.jpg" width="900" height="700"></div>
+        <div><img src="assets/depan/images/akred-nas-inter.jpg" width="900" height="700"></div>
 
         <div class="owl-carousel owl-classic owl-dots-secondary" data-items="1" data-sm-items="2" data-md-items="3" data-margin="5">
             
             <!-- Thumbnail Classic-->
             <article>
-                <div><img src="assets/depan/images/gallery-image-1-270x195.jpg" width="620" height="400"></div>
+                <div><img src="assets/depan/images/persentasi-akred-univ.jpg" width="620" height="400"></div>
             </article>
 
             <!-- Thumbnail Classic-->
