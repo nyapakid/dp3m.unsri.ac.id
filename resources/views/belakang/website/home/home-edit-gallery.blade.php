7c41c4c78624cb6552ef04ae56fd8f53ff0cbb8f
nyapakid <putrayudha1412@unsri.ac.id>
2026-05-22 16:06:31 +0700

    belakang progress

--- resources/views/belakang/website/home/home-edit-gallery.blade.php
+++ resources/views/belakang/website/home/home-edit-gallery.blade.php
@@ -26,50 +26,132 @@
 
                 @include('belakang.main-module-view.bartop')
 
+                <!-- Button Banner -->
+                <div class="container-fluid">
+                    <!-- Page Heading -->
+                    <div class="container mt-6 center">
+                        <div><img src="{{ asset('assets/belakang/img/penjelasan-home-galeri.png') }}" width="100%" height="100%"></div>
+                    </div>
+                    <br>
 
-                <div class="container mt-4">
-                    <h3>Edit Gallery</h3>
-                    <button class="btn btn-primary mb-3">+ Tambah Gambar</button>
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h3 class="m-0 font-weight-bold text-primary">Edit Galeri</h3>
+                            <p>disarankan ukuran gambar berukuran/dengan rasio 16:9</p>
+                            <br>
+                            <a href="#" class="btn btn-primary btn-icon-split">
+                                <span class="icon text-white-50">
+                                    <i class="fas fa-arrow-right"></i>
+                                </span>
+                                <span class="text">Tambah Gambar</span>
+                            </a>
+                        </div>
 
-                    <table class="table table-bordered table-striped table-hover">
-                        <thead class="table-dark">
-                            <tr>
-                                <th>no</th>
-                                <th>Judul Gambar</th>
-                                <th>Preview</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>Judul dari db</td>
-                                <td>Preview dari bloob db</td>
-                                <td>
-                                    <button class="btn btn-warning btn-sm">Edit</button>
-                                    <button class="btn btn-danger btn-sm">Hapus</button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>Judul dari db</td>
-                                <td>Preview dari bloob db</td>
-                                <td>
-                                    <button class="btn btn-warning btn-sm">Edit</button>
-                                    <button class="btn btn-danger btn-sm">Hapus</button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>3</td>
-                                <td>Judul dari db</td>
-                                <td>Preview dari bloob db</td>
-                                <td>
-                                    <button class="btn btn-warning btn-sm">Edit</button>
-                                    <button class="btn btn-danger btn-sm">Hapus</button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                        
+
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th style="width: 5%; white-space: nowrap;">No</th>
+                                            <th style="width: 16%; white-space: nowrap;">Judul Gambar</th>
+                                            <th>Preview</th>
+                                            <th style="width: 16%; white-space: nowrap;">Aksi</th>
+                                        </tr>
+                                    </thead>
+                        
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>Judul Gambar dari DB</td>
+                                            <td>
+                                                <img src="{{ asset('assets/depan/images/01.jpg') }}" width="50%">
+                                            </td>
+                                            <td>
+                                                <a href="#" class="btn btn-warning btn-icon-split">
+                                                    <span class="text">ganti gambar</span>
+                                                </a>
+                                                <br>
+                                                <br>
+                                                <a href="#" class="btn btn-danger btn-icon-split">
+                                                    <span class="text">Hapus gambar</span>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>2</td>
+                                            <td>Judul Gambar dari DB</td>
+                                            <td>
+                                                <img src="{{ asset('assets/depan/images/02.jpg') }}" width="50%">
+                                            </td>
+                                            <td>
+                                                <a href="#" class="btn btn-warning btn-icon-split">
+                                                    <span class="text">ganti gambar</span>
+                                                </a>
+                                                <br>
+                                                <br>
+                                                <a href="#" class="btn btn-danger btn-icon-split">
+                                                    <span class="text">Hapus gambar</span>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>3</td>
+                                            <td>Judul Gambar dari DB</td>
+                                            <td>
+                                                <img src="{{ asset('assets/depan/images/03.jpg') }}" width="50%">
+                                            </td>
+                                            <td>
+                                                <a href="#" class="btn btn-warning btn-icon-split">
+                                                    <span class="text">ganti gambar</span>
+                                                </a>
+                                                <br>
+                                                <br>
+                                                <a href="#" class="btn btn-danger btn-icon-split">
+                                                    <span class="text">Hapus gambar</span>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>4</td>
+                                            <td>Judul Gambar dari DB</td>
+                                            <td>
+                                                <img src="{{ asset('assets/depan/images/04.jpg') }}" width="50%">
+                                            </td>
+                                            <td>
+                                                <a href="#" class="btn btn-warning btn-icon-split">
+                                                    <span class="text">ganti gambar</span>
+                                                </a>
+                                                <br>
+                                                <br>
+                                                <a href="#" class="btn btn-danger btn-icon-split">
+                                                    <span class="text">Hapus gambar</span>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>5</td>
+                                            <td>Judul Gambar dari DB</td>
+                                            <td>
+                                                <img src="{{ asset('assets/depan/images/05.jpg') }}" width="50%">
+                                            </td>
+                                            <td>
+                                                <a href="#" class="btn btn-warning btn-icon-split">
+                                                    <span class="text">ganti gambar</span>
+                                                </a>
+                                                <br>
+                                                <br>
+                                                <a href="#" class="btn btn-danger btn-icon-split">
+                                                    <span class="text">Hapus gambar</span>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
             </div>    
