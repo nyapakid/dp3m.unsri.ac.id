c3cf3a605a519169ff0ebdbcb9a5ea1fd9c971c9
nyapakid <putrayudha1412@unsri.ac.id>
2026-05-25 12:06:40 +0700

    data auditor mk.2

--- resources/views/depan/spmi-siklus-view-module/tabelauditor.blade.php
+++ resources/views/depan/spmi-siklus-view-module/tabelauditor.blade.php
@@ -9,287 +9,274 @@
         <tbody>
             <tr>
                 <td>1</td>
-                <td>Dr. Ahmad Maulana, S.E., M.M.</td>
-                <td>Ekonomi</td>
+                <td>Ahmad Fikriyansyah, S.E., M.Ak</td>
+                <td>EKONOMI</td>
             </tr>
             <tr>
                 <td>2</td>
-                <td>Dr. Sukanto, S.E., M.Si.</td>
-                <td>Ekonomi</td>
+                <td>Ahmad Khoirun Najib, S.Fil., M.B.A.</td>
+                <td>EKONOMI</td>
             </tr>
             <tr>
                 <td>3</td>
-                <td>Dr. Yunisvita, S.E., M.Si.</td>
-                <td>Ekonomi</td>
+                <td>Muhammad Ridho Sahputra, S.T., M.M.</td>
+                <td>EKONOMI</td>
             </tr>
             <tr>
                 <td>4</td>
-                <td>Dr. Shelfi Malinda, S.E., M.M.</td>
-                <td>Ekonomi</td>
+                <td>Dolly Tanzil, M.Si.</td>
+                <td>EKONOMI</td>
             </tr>
             <tr>
                 <td>5</td>
-                <td>Agil Novriansya, SE.,M.Sc.,Ak.,CA.</td>
-                <td>Ekonomi</td>
+                <td>Dr. Sri Handayani, S.H., M.Hum</td>
+                <td>HUKUM</td>
             </tr>
+            <tr>
                 <td>6</td>
-                <td>Dr. Ichsan Hamidi, S.H.I., M.Si.</td>
-                <td>Ekonomi</td>
+                <td>Helena Primadianti Sulistyaningrum, S.H., M.H.</td>
+                <td>HUKUM</td>
             </tr>
             <tr>
                 <td>7</td>
-                <td>Dr. Suci Flambonita, S.H., M.H.</td>
-                <td>Hukum</td>
+                <td>Dr. Hamonangan Albariansyah, S.H., M.H.</td>
+                <td>HUKUM</td>
             </tr>
             <tr>
                 <td>8</td>
-                <td>Dr. Putu Samawati, S.H., M.H.</td>
-                <td>Hukum</td>
+                <td>Dedeng, S.H., M.H.</td>
+                <td>HUKUM</td>
             </tr>
             <tr>
                 <td>9</td>
-                <td>Helena Primadenti Sulistyaningrum, S.H., M.H.</td>
-                <td>Hukum</td>
+                <td>Desia Rakhma Banjarani, S.H., M.H.</td>
+                <td>HUKUM</td>
             </tr>
             <tr>
                 <td>10</td>
-                <td>Dr. Ridwan, SH.,M.Hum.</td>
-                <td>Hukum</td>
+                <td>Enggal Nurisman, S.T., M.T.</td>
+                <td>TEKNIK</td>
             </tr>
             <tr>
                 <td>11</td>
-                <td>Agus Ngadino, SH.,MH.</td>
-                <td>Hukum</td>
+                <td>Rhaptyalyani, S.T., M.Eng., Ph.D</td>
+                <td>TEKNIK</td>
             </tr>
             <tr>
                 <td>12</td>
-                <td>Dr. David Bahrin, S.T., M.T.</td>
-                <td>Teknik</td>
+                <td>M. A. Ade Saputra, S.T., M.T.</td>
+                <td>TEKNIK</td>
             </tr>
             <tr>
                 <td>13</td>
-                <td>Qomarul Hadi, S.T., M.T.</td>
-                <td>Teknik</td>
+                <td>Ir. Puspa Kurniasari, S.T., M.T.</td>
+                <td>TEKNIK</td>
             </tr>
             <tr>
                 <td>14</td>
-                <td>Dr. Wienty Triyuly, S.T., M.T.</td>
-                <td>Teknik</td>
+                <td>Ir. Eva Oktarinasari, S.T., M.T.</td>
+                <td>TEKNIK</td>
             </tr>
             <tr>
                 <td>15</td>
-                <td>Abdul Haris Dalimunthe, ST.,M.TI.</td>
-                <td>Teknik</td>
+                <td>dr. Masayu Syarinta Adenina, M.Biomed</td>
+                <td>KEDOKTERAN</td>
             </tr>
             <tr>
                 <td>16</td>
-                <td>Dr. Subandrate, M.Biomed</td>
-                <td>Kedokteran</td>
+                <td>Masayu Farah Diba, S.Si., M.Biomed</td>
+                <td>KEDOKTERAN</td>
             </tr>
             <tr>
-                <td>17</td>
-                <td>Dr. Nita Farisa, M. Biomed.</td>
-                <td>Kedokteran</td>
-            </tr>
-            <tr>
-                <td>18</td>
-                <td>Dr. Puji Rizki Suryani, M.Kes</td>
-                <td>Kedokteran</td>
-            </tr>
-            <tr>
-                <td>19</td>
-                <td>dr. Diyas Sayuki Ikhsan, Sp.K.J.</td>
-                <td>Kedokteran</td>
+                <td>17</ td>
+                <td>dr.Bintang Arroyantri Prananjaya, Sp.KJ</td>
+                <td>KEDOKTERAN</td >
+            </tr >
+            <tr >
+                <td >18</ td >
+                <td >Dr. dr. Dalilah, M.Kes.</td >
+                <td >KEDOKTERAN</td >
+            </tr >
+            <tr >
+                <td >19</td>
+                <td>dr. Puji Rizki Suryani, M.Kes.</td >
+                <td>KEDOKTERAN</td>
             </tr>
             <tr>
                 <td>20</td>
-                <td>dr. Veny Larasati, M.Biomed</td>
-                <td>Kedokteran</td>
+                <td>Dr. Arjuna Neni Triana, S.TP., M.Si.</td>
+                <td>PERTANIAN</td>
             </tr>
             <tr>
                 <td>21</td>
-                <td>dr. Masayu Syarinta Adenina, M.Biomed.</td>
-                <td>Kedokteran</td>
+                <td>Dr. Selly Oktarina, S.P., M.Si.</td>
+                <td>PERTANIAN</td>
             </tr>
             <tr>
                 <td>22</td>
-                <td>dr. Bintang Arroyantri Prananjaya, Sp.KJ.</td>
-                <td>Kedokteran</td>
+                <td>Fitra Yosi, S.Pt., M.S., Ph.D</td>
+                <td>PERTANIAN</td>
             </tr>
             <tr>
                 <td>23</td>
-                <td>Prof. Dr. Ir. Rujito Agus Suwignyo, M.Agr.</td>
-                <td>Pertanian</td>
+                <td>Dr. Ade Dwi Sasanti. S.Pi.,  M.Si.</td>
+                <td>PERTANIAN</td>
             </tr>
             <tr>
                 <td>24</td>
-                <td>Dr. Ir. Suparman SHK.</td>
-                <td>Pertanian</td>
+                <td>Dr. Astuti Kurnianingsih, S.P., M.Si.</td>
+                <td>PERTANIAN</td>
             </tr>
             <tr>
                 <td>25</td>
-                <td>Ir. Nura Malahayati, M.Sc., Ph.D.</td>
-                <td>Pertanian</td>
+                <td>Dr. Yosef, M.A.</td>
+                <td>KIP</td>
             </tr>
             <tr>
                 <td>26</td>
-                <td>Dr. Afnur Imsya, S.Pt., M.Si</td>
-                <td>Pertanian</td>
+                <td>Dr. Rahmi Susanti, M.Si.</td>
+                <td>KIP</td>
             </tr>
             <tr>
                 <td>27</td>
-                <td>Tanbiyaskur, S.Pi., M.Si.</td>
-                <td>Pertanian</td>
+                <td>Dr. Muhammad Yusup, S.Pd., M.Pd</td>
+                <td>KIP</td>
             </tr>
             <tr>
-                <td>28</ td>
-                <td>Indah Widiastuti, S.Pi., M.Si., Ph.D.</td>
-                <td>Pertanian</td>
+                <td>28</td>
+                <td>Dr. Syarifuddin, S.Pd., M.Pd</td>
+                <td>KIP</td>
             </tr>
-            <tr >
+            <tr>
                 <td>29</td>
-                <td>Dr. Saridan Markos, S., M.Si., M.Pd.</td>
-                <td>Keguruan dan Ilmu Pendidikan</td>
+                <td>Dr. Fitria Aprilia, M.Pd</td>
+                <td>KIP</td>
             </tr>
             <tr>
                 <td>30</td>
-                <td>Dr. Yosef, M.A</td>
-                <td>Keguruan dan Ilmu Pendidikan</td>
+                <td>Prof. Dr. Ridhah Taqwa</td>
+                <td>ISIP</td>
             </tr>
             <tr>
-                <td>31</td>
-                <td>Drs. A. Rachman Ibrahim, M.Sc.,Ph.D.</td>
-                <td>Keguruan dan Ilmu Pendidikan</td>
-            </tr>
+                <td>31</ td >
+                <td>Dr. Dadang Hikmah Purnama, M.Hum</td>
+                <td>ISIP</td>
+            </tr >
             <tr>
                 <td>32</td>
-                <td>Drs. Abdurrahman Ibrahim, M.Ed.</td>
-                <td>Keguruan dan Ilmu Pendidikan</td>
+                <td>Dr. Rudy Kurniawan, S.Th.I., M.Si.</td>
+                <td>ISIP</td>
             </tr>
             <tr>
                 <td>33</td>
-                <td>Dr. Masagus Mhd. Tibrani, S.Pd., M.Si.</td>
-                <td>Keguruan dan Ilmu Pendidikan</td>
+                <td>Dr. Katriza Imania, M.Si.</td>
+                <td>ISIP</td>
             </tr>
             <tr>
                 <td>34</td>
-                <td>Dr. Muhamad Yusup, S.Pd., M.Pd.</td>
-                <td>Keguruan dan Ilmu Pendidikan</td>
+                <td>Rindang Senja Andarini, S.I.Kom., M.I.Kom</td>
+                <td>ISIP</td>
             </tr>
             <tr>
                 <td>35</td>
-                <td>Dr. rer.nat. Indra Yustian, M.Si.</td>
-                <td>Matematika dan Ilmu Pengetahuan Alam</td>
+                <td>Alfensi Faruk, S.Si., M.Sc., Ph.D</td>
+                <td>MIPA</td>
             </tr>
             <tr>
                 <td>36</td>
-                <td>Dr. Fitri Maya Puspita, M.Si</td>
-                <td>Matematika dan Ilmu Pengetahuan Alam</td>
+                <td>Dr. Erry Koriyanti, S.Si., M.T.</td>
+                <td>MIPA</td>
             </tr>
             <tr>
                 <td>37</td>
-                <td>Singgih Tri Wardana, S.Si. M.Si.</td>
-                <td>Matematika dan Ilmu Pengetahuan Alam</td>
+                <td>Singgih Tri Wardana, S.Si., M.Si.</td>
+                <td>MIPA</td>
             </tr>
             <tr>
                 <td>38</td>
-                <td>Prof. Dr. Poedji Loekitowati H, M.Si.</td>
-                <td>Matematika dan Ilmu Pengetahuan Alam</td>
+                <td>Dr. Riris Aryawati, S.T., M.Si.</td>
+                <td>MIPA</td>
             </tr>
             <tr>
                 <td>39</td>
-                <td>Hoirun Nisyak, S.Pd., M.Pd.</td>
-                <td>Ilmu Sosial dan Ilmu Politik</td>
+                <td>Dr. Widia Purwaningrum, S.Si., M.Si.</td>
+                <td>MIPA</td>
             </tr>
             <tr>
                 <td>40</td>
-                <td>Dr. Dadang Himah Purnama, M. Hum.</td>
-                <td>Ilmu Sosial dan Ilmu Politik</td>
+                <td>Najwa Annuria Fithri, S.Farm., M.Sc., Ph.D</td>
+                <td>MIPA</td>
             </tr>
             <tr>
                 <td>41</td>
-                <td>Dr. Ridhah, M.Si.</td>
-                <td>Ilmu Sosial dan Ilmu Politik</td>
+                <td>Dr. Sutarno, M.T.</td>
+                <td>ILKOM</td>
             </tr>
             <tr>
                 <td>42</td>
-                <td>Kirsna Murti, S.I.Kom., MA.</td>
-                <td>Ilmu Sosial dan Ilmu Politik</td>
+                <td>Dr. Ali Ibrahim, M.Kom.</td>
+                <td>ILKOM</td>
             </tr>
             <tr>
                 <td>43</td>
-                <td>Dr. Katriza Imania, M.Si.</td>
-                <td>Ilmu Sosial dan Ilmu Politik</td>
+                <td>Dian Palupi Rini, Ph.D</td>
+                <td>ILKOM</td>
             </tr>
-            <tr>
+             <tr>
                 <td>44</td>
-                <td>Rindang Andarini, S.I.Kom.,M.I.Kom.</td>
-                <td>Ilmu Sosial dan Ilmu Politik</td>
+                <td>Endang Lestari, M.Kom.</td>
+                <td>ILKOM</td>
             </tr>
             <tr>
                 <td>45</td>
-                <td>Dr. Erwin, S.Si., M.Si.</td>
-                <td>Ilmu Komputer</td>
+                <td>Alvy Syahrini, M.Kom.</td>
+                <td>ILKOM</td>
             </tr>
             <tr>
                 <td>46</td>
-                <td>Hardini Novianti, M.T.</td>
-                <td>Ilmu Komputer</td>
+                <td>Dr. Novrikasari, S.KM., M.Kes</td>
+                <td>KESMAS</td>
             </tr>
             <tr>
                 <td>47</td>
-                <td>Rossi Passarella, S.T., M.Eng.</td>
-                <td>Ilmu Komputer</td>
+                <td>Dr. Fatmalina Febry, S.KM., M.Si.</td>
+                <td>KESMAS</td>
             </tr>
             <tr>
                 <td>48</td>
-                <td>Dr. Abdiansah, S.Kom., M.Cs.</td>
-                <td>Ilmu Komputer</td>
+                <td>Widya Lionita, S.KM., M.Ph</td>
+                <td>KESMAS</td>
             </tr>
             <tr>
                 <td>49</td>
-                <td>Dr. Ali Ibrahim, S.Kom., M.T.</td>
-                <td>Ilmu Komputer</td>
+                <td>Poppy Fujianti, S.KM., M.Sc.</td>
+                <td>KESMAS</td>
             </tr>
             <tr>
                 <td>50</td>
-                <td>Prof. Dr. Ir. Bambang Tutuko, MT.</td>
-                <td>Ilmu Komputer</td>
+                <td>Inoy Trisnaini, S.KM., M.KL.</td>
+                <td>KESMAS</td>
             </tr>
             <tr>
-                <td>51</td>
-                <td>Endang Lestari Ruskan, S.Kom.,MT.</td>
-                <td>Ilmu Komputer</td>
-            </tr >
+                <td>51</ td >
+                <td>Dr. Ir. Mery Hasmeda, M.Agr.</td>
+                <td>PASCASAEJANA</td>
+            </tr>
             <tr>
                 <td>52</td>
-                <td>Dr. Novikasari, S.K.M., M.Kes.</td>
-                <td>Kesehatan Masyarakat</td>
+                <td>Dr. Dade Jubaedah, S.Pi., M.Si.</td>
+                <td>PASCASAEJANA</td>
             </tr>
             <tr>
                 <td>53</td>
-                <td>Widya Lionita, S.K.M., M.PH.</td>
-                <td>Kesehatan Masyarakat</td>
+                <td>Elda Melwita, S.T.,M.T.,Ph.D.</td>
+                <td>PASCASAEJANA</td>
             </tr>
             <tr>
                 <td>54</td>
-                <td>Desheila Andarini, S.KM., M.Sc.</td>
-                <td>Kesehatan Masyarakat</td>
+                <td>Prof. Dr. Fakhili Gulo</td>
+                <td>PASCASAEJANA</td>
             </tr>
-            <tr>
-                <td>55</td>
-                <td>Dr. Risky Tirta Adiguna, S.TP., M.Si.</td>
-                <td>Pascasarjana</td>
-            </tr>
-            <tr>
-                <td>56</td>
-                <td>Arjuna Neni Triana, S.TP., M.Si.</td>
-                <td>Pascasarjana</td>
-            </tr>
-            <tr>
-                <td>57</td>
-                <td>Sabri Sudirman, S.Pi.,M.Si.,Ph.D.</td>
-                <td>Pascasarjana</td>
-            </tr>
+
         </tbody>
     </table>
